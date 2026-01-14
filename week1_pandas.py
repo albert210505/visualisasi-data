cdea16718fbfb2bdcdf93f810fafe71691067bc0
albert210505 <albertzidane97@gmail.com>
2026-01-15 02:28:46 +0700

    Week 1 Pandas: seleksi baris DataFrame

--- week1_pandas.py
+++ week1_pandas.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-n_rows = 5
+n_rows = 10
 n_cols = 5
 cols = tuple('ABCDE')
 
@@ -11,7 +11,7 @@ df = pd.DataFrame(np.random.randint(1, 10, size=(n_rows, n_cols)),
 print("Dataframe awal :")
 print(df)
 
-
+# VIDEO 1: PREFIX & SUFFIX
 df_prefix = df.add_prefix('kolom_')
 print("DataFrame dengan prefix:")
 print(df_prefix)
@@ -20,3 +20,9 @@ print(df_prefix)
 df_suffix = df.add_suffix('_field')
 print("DataFrame dengan suffix:")
 print(df_suffix)
+
+# VIDEO 2: SELEKSI BARIS
+
+df_seleksi = df[~df['A'].isin([1, 3])]
+print("Seleksi baris")
+print(df_seleksi)
